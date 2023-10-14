cdf4b3d8f4b7d5d1fb8c623fc8c4d0c583921a2e
Llimy1 <alsgur990104@gmail.com>
2023-10-14 21:56:25 +0900

    Initial commit

---
+++ src/main/java/com/example/distribution_test/DistributionTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.distribution_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionTestApplication.class, args);
+    }
+
+}
