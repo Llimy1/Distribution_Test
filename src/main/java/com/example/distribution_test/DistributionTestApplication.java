5f916f56791b6b8afeaa41c9f3008297eae18528
Llimy1 <alsgur990104@gmail.com>
2023-10-17 22:56:12 +0900

    feat : 배포 테스트

--- src/main/java/com/example/distribution_test/DistributionTestApplication.java
+++ src/main/java/com/example/distribution_test/DistributionTestApplication.java
@@ -2,12 +2,21 @@ package com.example.distribution_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class DistributionTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DistributionTestApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String handleRequest() {
+        return "ok";
     }
 
 }
