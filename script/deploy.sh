807774b5663608d1402f4c7da196aa7f7109b5dc
Llimy1 <alsgur990104@gmail.com>
2023-10-17 23:09:27 +0900

    feat : 배포 테스트

--- script/deploy.sh
+++ script/deploy.sh
@@ -32,16 +32,16 @@ if [ -z $IS_GREEN  ];then # blue라면
   echo "2. green container up"
   docker-compose up -d green # 컨테이너 실행
 
-#  while [ 1 = 1 ]; do
-#  echo "3. green health check..."
-#  sleep 3
-#
-#  REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
-#    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
-#            echo "health check success"
-#            break ;
-#            fi
-#  done;
+  while [ 1 = 1 ]; do
+  echo "3. green health check..."
+  sleep 3
+
+  REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
+    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
+            echo "health check success"
+            break ;
+            fi
+  done;
 
   echo "4. reload nginx"
   sudo cp /etc/nginx/nginx.green.conf /etc/nginx/nginx.conf
@@ -58,16 +58,16 @@ else
   echo "2. blue container up"
   docker-compose up -d blue
 
-#  while [ 1 = 1 ]; do
-#    echo "3. blue health check..."
-#    sleep 3
-#    REQUEST=$(curl http://127.0.0.1:8081) # blue로 request
-#
-#    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
-#      echo "health check success"
-#      break ;
-#    fi
-#  done;
+  while [ 1 = 1 ]; do
+    echo "3. blue health check..."
+    sleep 3
+    REQUEST=$(curl http://127.0.0.1:8081) # blue로 request
+
+    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
+      echo "health check success"
+      break ;
+    fi
+  done;
 
   echo "4. reload nginx"
   sudo cp /etc/nginx/nginx.blue.conf /etc/nginx/nginx.conf
