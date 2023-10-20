3b0047bf89d0e7d4150311c06f7122391c0bc964
Llimy1 <alsgur990104@gmail.com>
2023-10-20 23:41:09 +0900

    feat : 배포 테스트

--- src/main/java/com/example/distribution_test/config/S3Config.java
+++ src/main/java/com/example/distribution_test/config/S3Config.java
@@ -12,13 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${AWS_ACCESS_KEY}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${AWS_SECRET_KEY}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${AWS_BUCKET}")
     private String region;
 
     @Bean
